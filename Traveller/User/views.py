939f5e79e1836f455ebcf4b8f56e7e249bf69c18
unknown <AJSO>
2020-12-03 22:57:39 +0400

    token genrated

--- Traveller/User/views.py
+++ Traveller/User/views.py
@@ -7,4 +7,30 @@ from rest_framework import permissions
 
 # Create your views here.
 class SignupView(APIView):
-    pass
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        #getting post contents from the form 
+        fullname = data['full_name']
+        email = data['email']
+        phone = data['phone']
+        password = data['password']
+        password2 = data['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                return Response({'error':'Email already exists'})
+            else:
+                if len(password) < 6:
+                    return Response({'error': 'Password must be at least 6 characters'})
+                else:
+                    user = User.objects.create_user(full_name=fullname, email=email, phone=phone, password=password)
+
+                    user.save()
+                    return Response({'success': 'User created successfully'})
+        else:
+            return Response({'error': 'Passwords do not match'})
+
+

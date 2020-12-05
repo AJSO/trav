d6b8f616c6cc8e3f5fd6a8c4ca07a58ff80c6e1b
Frank <frankjay2017@gmail.com>
2020-12-05 12:30:32 +0400

    Added git ignore

---
+++ User/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core.validators import RegexValidator
+
+
+class UserAccountManager(BaseUserManager):
+
+    #Creating Normal User
+    def create_user(self, email, full_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('Email Address is required'))
+        
+        if not full_name:
+            raise ValueError(_('Full name is required'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, full_name=full_name, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    #creating a superuser. (No need to add phone number, it will be required automatically)
+    def create_superuser(self, email, full_name, password, **other_fields):
+
+        
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        user = self.create_user(email, full_name, password, **other_fields)
+
+        return user
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+
+    phone_regex = RegexValidator(regex = r'^\+?1?\d{9,14}$', message = "Phone number must be entered in the format: '+1111111111'. Up to 14 digits allowed.")
+
+    email = models.EmailField(_('email address'), unique=True)
+    full_name = models.CharField(max_length=150, blank=True)
+    phone = models.CharField(validators=[phone_regex], max_length=17, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name', 'phone']
+
+    def __str__(self):
+        return f"{self.full_name} {self.phone}"
+
+    #function to store the otp requests
+class PhoneOTP(models.Model):
+
+    phone_regex = RegexValidator( regex = r'^\+?1?\d{9,14}$', message = "Phone number must be entered in the format: '+111111111'. Up to 14 digits allowed.")
+
+    phone       = models.CharField(validators=[phone_regex], max_length=17, unique=True)
+    otp         = models.CharField(max_length = 9, blank = True, null= True)
+    count       = models.IntegerField(default = 0, help_text = 'Number of otp sent')
+    logged      = models.BooleanField(default = False, help_text = 'If otp verification got successful')
+    forgot      = models.BooleanField(default = False, help_text = 'only true for forgot password')
+    forgot_logged = models.BooleanField(default = False, help_text = 'Only true if validdate otp forgot get successful')
+
+
+    def __str__(self):
+        return str(self.phone) + ' is sent ' + str(self.otp)
